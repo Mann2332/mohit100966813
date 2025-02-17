778d28cd5ba6b38f38117aaef0519b16f7876c8b
Mann2332 <mohitdodiya2332@gmail.com>
2025-02-16 20:07:11 -0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const app = express();
 
-// Set PORT to use the environment variable (Cloud Run requires this)
 const PORT = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
-  res.send("Hello from Cloud Run!");
+    res.send("Hello, Cloud Run!");
 });
 
-// Start the server on the correct port
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
