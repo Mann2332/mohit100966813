9efa350efe458f39bec3d0f81b0130ca4bdc018a
Mann2332 <mohitdodiya2332@gmail.com>
2025-02-16 21:30:21 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,3 @@
-const express = require('express');
-const app = express();
-
-const PORT = process.env.PORT || 8080;
-
-app.get('/api', (req, res) => {
-    res.send('API is working!');
-});
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.get("/", (req, res) => {
+    res.send("Welcome to My API!");
 });
