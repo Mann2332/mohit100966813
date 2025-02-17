c23514dbdd3496666023fdb9c63cc73bfdc8fd4a
Mann2332 <mohitdodiya2332@gmail.com>
2025-02-16 21:18:28 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,32 +1,12 @@
 const express = require('express');
 const app = express();
-const db = require('./persistence');
-const getItems = require('./routes/getItems');
-const addItem = require('./routes/addItem');
-const updateItem = require('./routes/updateItem');
-const deleteItem = require('./routes/deleteItem');
 
-app.use(express.json());
-app.use(express.static(__dirname + '/static'));
+const PORT = process.env.PORT || 8080;
 
-app.get('/items', getItems);
-app.post('/items', addItem);
-app.put('/items/:id', updateItem);
-app.delete('/items/:id', deleteItem);
-
-db.init().then(() => {
-    app.listen(3000, () => console.log('Listening on port 3000'));
-}).catch((err) => {
-    console.error(err);
-    process.exit(1);
+app.get('/api', (req, res) => {
+    res.send('API is working!');
 });
 
-const gracefulShutdown = () => {
-    db.teardown()
-        .catch(() => {})
-        .then(() => process.exit());
-};
-
-process.on('SIGINT', gracefulShutdown);
-process.on('SIGTERM', gracefulShutdown);
-process.on('SIGUSR2', gracefulShutdown); // Sent by nodemon
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
